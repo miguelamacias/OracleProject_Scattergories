1b76a87d6ab4e3d5d7588aeaca789e59d8695790
Macis <Admin@PC-Angel.mshome.net>
2019-02-28 12:52:51 +0100

    Primera versión de la clase Dado terminada
    Se ha añadido un constructor que inicialize las variables de instancia
    en vez de hacerlo en el momento de declararlas. Se ha añadido un método
    que reestablece las letras usadas para empezar una nueva partida.

--- ProyectoOracle_Juego/src/application/Dado.java
+++ ProyectoOracle_Juego/src/application/Dado.java
@@ -5,18 +5,34 @@ import java.security.SecureRandom;
 public class Dado {
 	//Variables de instancia
 	private SecureRandom dado = new SecureRandom();
-	private char[] letras = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
-	private boolean[] letrasUsadas = {false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false};
+	private char[] letras;
+	private String alfabeto;
+	private boolean[] letrasUsadas;
 
+	public Dado() {
+		alfabeto = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+		letras = alfabeto.toCharArray();
+		letrasUsadas = new boolean[letras.length];
+		restablecerLetrasUsadas();
+
+	}
 
 	public char siguienteLetra () {
 		int tirada = dado.nextInt(26);
 		char resultado = letras[tirada];
+
 		if (!letrasUsadas[tirada]) {
 			letrasUsadas[tirada] = true;
 			return resultado;
+
 		} else {
 			return siguienteLetra();
+		}
+	}
+
+	public void restablecerLetrasUsadas() {
+		for (int i = 0; i < letrasUsadas.length; i++) {
+			letrasUsadas[i] = false;
 		}
 	}
 }
