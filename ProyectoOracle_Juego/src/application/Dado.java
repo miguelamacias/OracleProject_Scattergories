639c0998b0efdb53c49b38b463cd6a55ce71a024
Macis <Admin@PC-Angel.mshome.net>
2019-03-01 11:25:47 +0100

    Javadoc añadido

--- ProyectoOracle_Juego/src/application/Dado.java
+++ ProyectoOracle_Juego/src/application/Dado.java
@@ -2,14 +2,23 @@ package application;
 
 import java.security.SecureRandom;
 
+/**
+ * Clase que simula el tipico dado de letras del Scattergories.
+ * @author Miguel Angel Macias
+ */
 public class Dado {
 	//Variables de instancia
-	private SecureRandom dado = new SecureRandom();
+	private SecureRandom dado;
 	private char[] letras;
 	private String alfabeto;
 	private boolean[] letrasUsadas;
 
+	/**
+	 * Constructor sin parametros que inicializa todas las variables de instancia
+	 * necesarias para el funcionamiento de la clase.
+	 */
 	public Dado() {
+		dado = new SecureRandom();
 		alfabeto = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 		letras = alfabeto.toCharArray();
 		letrasUsadas = new boolean[letras.length];
@@ -17,6 +26,11 @@ public class Dado {
 
 	}
 
+	/**
+	 * Metodo que simula una tirada del dado y devuelve la letra obtenida.
+	 * Si la letra es repetida se descarta y se vuelve a tirar el dado.
+	 * @return Una letra del abecedario no devuelta anteriormente.
+	 */
 	public char siguienteLetra () {
 		int tirada = dado.nextInt(26);
 		char resultado = letras[tirada];
@@ -30,12 +44,19 @@ public class Dado {
 		}
 	}
 
+	/**
+	 * Limpia la lista de letras usadas para empezar de nuevo.
+	 */
 	public void restablecerLetrasUsadas() {
 		for (int i = 0; i < letrasUsadas.length; i++) {
 			letrasUsadas[i] = false;
 		}
 	}
 
+	/**
+	 * Usado para que no se tire el dado mas veces que numero de letras diferentes hay.
+	 * @return El numero de letras diferentes disponibles
+	 */
 	public int getCantidadLetras() {
 		return letras.length;
 	}
