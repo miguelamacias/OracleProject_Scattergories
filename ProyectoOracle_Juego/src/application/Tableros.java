ece34537f4484d9a26a1377ab3f9a4f6edd23454
Macis <Admin@PC-Angel.mshome.net>
2019-03-25 17:21:19 +0100

    Segundo tablero finalizado. RC01

--- ProyectoOracle_Juego/src/application/Tableros.java
+++ ProyectoOracle_Juego/src/application/Tableros.java
@@ -10,8 +10,10 @@ import javafx.scene.control.TextArea;
 public class Tableros {
 	//Variables para los tableros
 	private String clasico, naturaleza, scattergories1;
-	private String cabeceraClasico, CabeceraNaturaleza, CabeceraScattergories1;
-	private String[] soluciones1 = new String[27];
+	private String cabeceraClasico, cabeceraNaturaleza, cabeceraScattergories1;
+	private String[] solucionesClasico = new String[27];
+	private String[] solucionesNaturaleza = new String[27];
+	private String[] solucionesScattergories1 = new String[27];
 	FileReader fr;
 	BufferedReader leer;
 	File soluciones;
@@ -22,21 +24,29 @@ public class Tableros {
     	naturaleza = "Naturaleza: Mamífero, Ave, Reptil, Planta, Anfibio, Invertebrado, Órgano animal";
     	scattergories1= "Scattergories uno: Animal de compañia, etc, cosas frias, etx, muchas cosas";
     	
-    	CabeceraNaturaleza = "Mamífero\t\t\t Ave\t\t\t Reptil\t\t\t Planta\t\t\t Anfibio\t\t\t Invertebrado\t\t\t Órgano animal\n";
-    	CabeceraScattergories1 = "Aun por determinar\n";
-    	
-    	//Cargar las soluciones desde el archivo txt
-    	soluciones = new File("src/res/soluciones1.txt");    	
+    	//Objetos necesarios para cargar las soluciones desde el archivo txt
+    	soluciones = new File("src/res/soluciones.txt");    	
 		
 		fr = new FileReader(soluciones);
 		leer = new BufferedReader(fr);
 		
 		//cargar cabeceras
 		cabeceraClasico = leer.readLine();
+		cabeceraNaturaleza = leer.readLine();
+		cabeceraScattergories1 = leer.readLine();
 		
-		for (int i = 0; i < soluciones1.length; i++) { 
-			soluciones1[i] = leer.readLine();
+		//cargar respuestas
+		for (int i = 0; i < solucionesClasico.length; i++) { 
+			solucionesClasico[i] = leer.readLine();
 		}
+		for (int i = 0; i < solucionesNaturaleza.length; i++) {
+			solucionesNaturaleza[i] = leer.readLine();
+		}
+		for (int i = 0; i < solucionesScattergories1.length; i++) {
+			solucionesScattergories1[i] = leer.readLine();
+		}
+		
+		
     	
     }
     
@@ -48,10 +58,12 @@ public class Tableros {
 			jugadaPC.appendText("\n");
 			break;
 		case 1:
-			jugadaPC.setText(CabeceraNaturaleza);
+			jugadaPC.setText(cabeceraNaturaleza);
+			jugadaPC.appendText("\n");
 			break;
 		case 2:
-			jugadaPC.setText(CabeceraScattergories1);
+			jugadaPC.setText(cabeceraScattergories1);
+			jugadaPC.appendText("\n");
 			break;
 		default:
 			break;
@@ -72,13 +84,17 @@ public class Tableros {
 		case 0:
 			jugadaPC.setText(cabeceraClasico);
 			jugadaPC.appendText("\n");
-	    	jugadaPC.appendText(String.format(soluciones1[letra]));
+	    	jugadaPC.appendText(String.format(solucionesClasico[letra]));
 			break;
 		case 1:
-			jugadaPC.setText("Solución aun no implementada\n");
+			jugadaPC.setText(cabeceraNaturaleza);
+			jugadaPC.appendText("\n");
+	    	jugadaPC.appendText(String.format(solucionesNaturaleza[letra]));
 			break;
 		case 2:
-			jugadaPC.setText("Solución aun no implementada\n");
+			jugadaPC.setText(cabeceraScattergories1);
+			jugadaPC.appendText("\n");
+	    	jugadaPC.appendText(String.format(solucionesScattergories1[letra]));
 			break;
 		default:
 			break;
